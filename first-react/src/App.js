6e985b98d09ef9fc58ced16c35a04c01679a93d9
theRobMay <robbie.maysent@gmail.com>
2022-10-18 21:01:23 -0500

    adding some basic functionality

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isClicked: false,
+            todos: [],
+            text: ''
+        };
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        console.log("I got clicked");
+        this.setState({
+            isClicked: !this.state.isClicked
+        })
+    }
+
+    render() {
+        console.log( "THIS -->", this)
+        return (
+            <div className="App">
+                <h1>{this.state.isClicked === true ? "I have been clicked" : "I have not been clicked"}</h1>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+        );
+    }
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         Hello World
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
